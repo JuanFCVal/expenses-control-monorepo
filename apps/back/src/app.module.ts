22640cd894dce298c97b740badcc2243f1060b43
JuanFCVal <cevallosjuanfrancisco@gmail.com>
2024-10-12 09:58:47 -0500

    :sparkles: set up connection with postgres

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -3,10 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'front/dist'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: parseInt(process.env.POSTGRES_PORT) | 5432,
+      username: process.env.POSTGRES_USER || 'your_username',
+      password: process.env.POSTGRES_PASSWORD || 'your_password',
+      database: process.env.POSTGRES_DB || 'your_database',
+      autoLoadEntities: true,
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
